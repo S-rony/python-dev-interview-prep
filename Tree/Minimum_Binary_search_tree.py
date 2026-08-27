2abd153fb5e5eec98af86019c8aadee310374089
Soubhik Sinha <91076835+S-rony@users.noreply.github.com>
2026-08-27 21:34:28 +0530

    Add files via upload

---
+++ Tree/Minimum_Binary_search_tree.py
@@ -0,0 +1,102 @@
+class Node:
+    def __init__(self,value):
+        self.left = None
+        self.right = None
+        self.data = value
+
+def insert(root,value):
+    if root is None:
+        return Node(value)
+    if root.data == value:
+        return root
+    if root.data > value:
+        root.left = insert(root.left,value)
+    else:
+        root.right = insert(root.right,value)
+    return root
+
+def search(root,value):
+    if root is None:
+        print("\nElement Not Found",end = "\n")
+        return
+    if root.data == value:
+        print("Element Found")
+        return
+    if root.data > value:
+        search(root.left,value)
+    else:
+        search(root.right,value)
+
+def get_successor(root):
+    root = root.right
+    while root is not None and root.left is not None:
+        root = root.left
+    return root #it will return inorder successor
+
+def delete(root,value):
+    if root is None:
+        return root
+    if root.data > value:
+        root.left = delete(root.left, value)
+    elif root.data < value:
+        root.right = delete(root.right, value)
+    else: #root == value , value mil gya
+        if root.left is None:
+            return root.right
+        elif root.right is None:
+            return root.left
+        else:
+            succ = get_successor(root)
+            root.data = succ.data
+            root.right = delete(root.right,succ.data)
+    return root
+
+def InOrder(root):
+    if root is not None:
+        InOrder(root.left)
+        print(root.data, end=" ")
+        InOrder(root.right)
+
+def mini(root):
+    if root is None:
+        return None
+
+    while root.left:
+        root = root.left
+    return root.data
+
+def maxi(root):
+    if root is None:
+        return None
+
+    while root.right:
+        root = root.right
+    return root.data
+
+
+
+
+root = insert(None,4)
+root = insert(root, 2)
+root = insert(root, 6)
+root = insert(root,1)
+root = insert(root, 3)
+root = insert(root, 5)
+root = insert(root, 7)
+
+# root = Node(20)
+# root.left = Node(15)
+# root.left.left = Node(12)
+# root.left.right = Node(18)
+# root.right = Node(30)
+# root.right.right = Node(40)
+
+InOrder(root)
+print()
+search(root,3)
+
+print("minimum", mini(root))
+
+print("maximum", maxi(root))
+# print("\n")
+
